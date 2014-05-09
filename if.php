2d45ed6226f71a856b60dca009ba1b183755217b
Mike Jaime <mikejaime@yahoo.com>
2014-05-09 14:46:58 -0500

    reformatted my code to make it easier to read

--- if.php
+++ if.php
@@ -1,72 +1,26 @@
 <?php
 
-/*
-$a = 5;
-$b = 10;
-$c = '10';
-
-if ($a == $b) {
-    // output the appropriate result
-    echo "$a is less than $b\n";
-}
-
-if ($b > $a) {
-    // output the appropriate result
-    echo "$b is greater than $a\n";
-}
-
-if ($b >= $c) {
-    // output the appropriate result
-    echo "$b is greater or equal to $c\n";
-}
-
-if ($b <= $c) {
-    // output the appropriate result
-    echo "$b is less than or equal to $c\n";
-}
-
-if ($b == $c) {
-    // output the appropriate result
-    echo "$b is equal to $c\n";
-}
-
-if ($b === $c) {
-    // output the appropriate result
-    echo "$b is not identical $c\n";
-}
-
-// output the appropriate result
-if ($b != $c) {
-    echo "$b is not equal to $c\n";
-}
-
-// output the appropriate result
-if ($b !== $c) {
-    echo "$b is not identical to $c\n";
-}
-*/
-
 $a = 34;
 $b = 56;
 $c = '56';
 
 // Add an else clause to the net 2 statements
-if ($a < $b) {
+if ($a < $b){
     // output the appropriate result
     echo "$a is less than $b\n";
-}   else{
-        echo "$a is not less than $b\n";
+}   else {
+    echo "$a is not less than $b\n";
 }
 
-if ($b > $a) {
+if ($b > $a){
     // output the appropriate result
     echo "$b is greater than $a\n";
-}   else{
-        echo "$b is not greater than $a\n";
+}   else {
+    echo "$b is not greater than $a\n";
 }
 
 // Shorten the next 2 statements into an if/else
-if ($b >= $c) {
+if ($b >= $c){
     // output the appropriate result
     echo "$b is greater or equal to $c\n";
 }   else {
