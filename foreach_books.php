52a1d1978f8be162be22a54830b12f7e21caae5a
Mike Jaime <mikejaime@yahoo.com>
2014-05-14 08:49:13 -0500

    implemented a loop that list each book title and its appending details

--- foreach_books.php
+++ foreach_books.php
@@ -1,5 +1,37 @@
 <?php 
 
+$books = array(
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    )
+);
 
+//Construct a loop that iterates through each book and then each 
+//book's keys and values. Have it output the book's title, then 
+//list the key value pairs for the data about each book.
+
+foreach ($books as $title => $details) {
+	echo $title . PHP_EOL;
+	echo " Realeased in " . $details[published] . PHP_EOL; 	
+	echo " Written by " . $details[author] . PHP_EOL;
+	echo " Pages " . $details[pages] . PHP_EOL;
+}
 
  ?>
