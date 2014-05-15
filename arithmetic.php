71c2edfab26a4574f9d9a3046356fd2c9ce0df84
Mike Jaime <mikejaime@yahoo.com>
2014-05-15 09:50:19 -0500

    2nd commmit - added new line to execution and called for each function

--- arithmetic.php
+++ arithmetic.php
@@ -1,19 +1,31 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b;
+    echo $a + $b . PHP_EOL;
 }
+
+add(4,5);
+
 
 function subtract($a, $b) {
-    echo $a - $b;
+    echo $a - $b . PHP_EOL;
 }
+
+subtract(456,567);
+
 
 function multiply($a, $b) {
-    echo $a * $b;
+    echo $a * $b . PHP_EOL;
 }
 
+multiply(45,2);
+
+
 function divide($a, $b) {
-    echo $a / $b;
+    echo $a / $b . PHP_EOL;
 }
+
+divide(56,3);
+
 
 ?>
