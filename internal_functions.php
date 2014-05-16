3b3778929b70629f909848cc2e9fb5df74c48b4c
Mike Jaime <mikejaime@yahoo.com>
2014-05-16 11:23:38 -0500

    initial commit - completed initial tasks, implmented isset and empty test on variables

---
+++ internal_functions.php
@@ -0,0 +1,31 @@
+<?php
+
+$nothing = NULL;
+$something = '';
+$array = array(1,2,3);
+
+// Create a function that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
+function setcheck($var) {			//$var could be named anything but acts as a place holder within the function
+    if (isset($var)) {
+        echo "\$variable is SET\n";
+    } elseif (empty($var)) {
+    	echo "\$variable is EMPTY\n";
+    }
+}
+
+// TEST: If var $nothing is set, display '$nothing is SET'
+
+// TEST: If var $nothing is empty, display '$nothing is EMPTY'
+
+// TEST: If var $something is set, display '$something is SET'
+setcheck($nothing);
+setcheck($something);
+setcheck($array);
+
+// Serialize the array $array, and output the results
+$serial = serialize($array);
+echo $serial;
+
+// Unserialize the array $array, and output the results
+$clone = unserialize($serial);
+print_r($clone);
