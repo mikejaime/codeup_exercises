002063a42c2de3421709f2d151f06505cb1aa704
Mike Jaime <mikejaime@yahoo.com>
2014-05-16 11:29:44 -0500

    updated program to check if the  variable is empty

--- internal_functions.php
+++ internal_functions.php
@@ -6,7 +6,7 @@ $array = array(1,2,3);
 
 // Create a function that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
 function setcheck($var) {			//$var could be named anything but acts as a place holder within the function
-    if (isset($var)) {
+    if (isset($var) && (!empty($var)) {
         echo "\$variable is SET\n";
     } elseif (empty($var)) {
     	echo "\$variable is EMPTY\n";
