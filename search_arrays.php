19daad3b8ee0413c8c68c9c8c4b7828591440f50
Mike Jaime <mikejaime@yahoo.com>
2014-05-19 14:24:20 -0500

    initial commit - created a function that returns true & false from a 0 index during an array search

---
+++ search_arrays.php
@@ -0,0 +1,27 @@
+<?php
+
+
+	
+
+// first names
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+//Create a function that returns TRUE or FALSE if an array value is found. Search for Tina and Bob in $names. Make sure it works as expected.
+function search($name, $names){
+	if (array_search($name, $names) !== false)
+	{
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+var_dump(search('Tina', $names));
+
+
+
+//Create a function to compare 2 arrays that returns the number of values in common between the arrays. Use the 2 example arrays and make //sure your solution uses array_search().
+
