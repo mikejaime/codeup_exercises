97c08758f6eb61ec8d2d701393f266ebea8dd4af
Mike Jaime <mikejaime@yahoo.com>
2014-05-20 14:29:49 -0500

    created humanized list of physicists

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,21 +1,23 @@
 <?php
 
 // Converts array into list n1, n2, ..., and n3
-function humanized_list($array) {
-  // Your solution goes here!
+function humanized_list($physicists_array) {
+	implode("/n", $physicists_array);
+	return;
 }
 
 // List of famous peeps
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
+//convereted string to array
 $physicists_array = explode(', ', $physicists_string);
 
+//removed last index and stored it in variable
 $last = array_pop($physicists_array);
-
-//  echo implode(', ', $physicist_array) . " and " . $last ;
 
 
 // Humanize that list
+// converted array back to a string and created human friendly string
 $famous_fake_physicists = implode(', ', $physicists_array) . " and " . $last ;
 
 // Output sentence
