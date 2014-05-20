a5a1c00b78e0c96b1f30e1d4785be6a4f7badaf8
Mike Jaime <mikejaime@yahoo.com>
2014-05-20 14:47:46 -0500

    sorted humanized list in alphabetical order

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,9 +1,9 @@
 <?php
 
 // Converts array into list n1, n2, ..., and n3
-function humanized_list($physicists_array) {
-	implode("/n", $physicists_array);
-	return;
+function humanized_list($array) {
+	sort($array);
+	return implode("\n", $array);
 }
 
 // List of famous peeps
@@ -12,6 +12,9 @@ $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mal
 //convereted string to array
 $physicists_array = explode(', ', $physicists_string);
 
+
+echo humanized_list($physicists_array) . PHP_EOL;
+
 //removed last index and stored it in variable
 $last = array_pop($physicists_array);
 
@@ -19,6 +22,7 @@ $last = array_pop($physicists_array);
 // Humanize that list
 // converted array back to a string and created human friendly string
 $famous_fake_physicists = implode(', ', $physicists_array) . " and " . $last ;
+
 
 // Output sentence
 echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.\n";
