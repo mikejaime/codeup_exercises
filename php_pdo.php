07c3543fa9f7fc55b6ba13498b9dbdb86cb228a6
Mike Jaime <mikejaime@yahoo.com>
2014-06-20 17:13:39 -0500

    initaial - commit created file that connects to mysql databas
    es

---
+++ php_pdo.php
@@ -0,0 +1,12 @@
+<?php
+
+// Get new instance of PDO object
+$dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_pdo_test_db', 'mike', 'letmein');
+
+// Tell PDO to throw exceptions on error
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+
+?>
